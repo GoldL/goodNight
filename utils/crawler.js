0751b5e46eba60e7125db4ee0e208bf70b958140
iGolden-win10 <136895062@qq.com>
2020-07-27 20:31:06 +0800

    修改文案

--- utils/crawler.js
+++ utils/crawler.js
@@ -16,7 +16,33 @@ function crawler (url, callback) {
   })
 }
 
-const url = 'https://baijiahao.baidu.com/s?id=1600401539542362481&wfr=spider&for=pc'
+// const url = 'https://baijiahao.baidu.com/s?id=1600401539542362481&wfr=spider&for=pc'
+
+// crawler(url, html => {
+//   if (!html) {
+//     console.log('爬取内容出错了')
+//     return
+//   }
+//   const $ = cheerio.load(html)
+//   $('.bjh-p', '#article').each(async (i, e) => {
+//     const el = $(e)
+//     let text = el.text()
+//     if (text.includes('NO')) {
+//       text = text.split('、')[1]
+//       if (!text.includes('晚安')) {
+//         text += '晚安！'
+//       }
+//       try {
+//         const goodNight = { content: text }
+//         await GoodNight.create(goodNight)
+//       } catch(e) {
+//         console.log(i, e)
+//       }
+//     }
+//   })
+// })
+
+const url = 'https://zhuanlan.zhihu.com/p/99190251'
 
 crawler(url, html => {
   if (!html) {
@@ -24,18 +50,19 @@ crawler(url, html => {
     return
   }
   const $ = cheerio.load(html)
-  $('.bjh-p', '#article').each(async (i, e) => {
+  $('p', '.Post-RichText').each(async (i, e) => {
+    console.log(i)
     const el = $(e)
     let text = el.text()
-    if (text.includes('NO')) {
-      text = text.split('、')[1]
+    if (text.includes('.')) {
+      text = text.split('.')[1]
       if (!text.includes('晚安')) {
-        text += '晚安！'
+        text += '   晚安！'
       }
       try {
-        const goodNight = { content: text }
-        await GoodNight.create(goodNight)
-      } catch(e) {
+        let goodNight = await GoodNight.findOne({ where: { id: i + 7 } })
+        await goodNight.update({ content: text })
+      } catch (e) {
         console.log(i, e)
       }
     }
