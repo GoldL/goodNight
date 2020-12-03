959a3e73f1ee50373620c0929ee3192d86cf950f
iGolden <136895062@qq.com>
2020-12-03 23:46:42 +0800

    新增情话语录

--- utils/crawler.js
+++ utils/crawler.js
@@ -44,7 +44,44 @@ function crawler(url, callback) {
 //   })
 // })
 
-const url = 'https://zhuanlan.zhihu.com/p/37383553'
+// const url = 'https://zhuanlan.zhihu.com/p/37383553'
+
+// crawler(url, html => {
+//   if (!html) {
+//     console.log('爬取内容出错了')
+//     return
+//   }
+//   const $ = cheerio.load(html)
+//   $('p', '.Post-RichText').each(async (i, e) => {
+//     const el = $(e)
+//     let text = el.text()
+//     if (text.includes('.') || text.includes('．')) {
+//       if (text.includes('.')) {
+//         text = text.split('.')[1]
+//       }
+//       if (text.includes('．')) {
+//         text = text.split('．')[1]
+//       }
+//       if (!text.includes('晚安')) {
+//         text += '   晚安！'
+//       }
+//       console.log(text)
+//       try {
+//         let goodNight = await GoodNight.findOne({ where: { id: i + 110 } })
+//         if (!goodNight) {
+//           const goodNight = { content: text }
+//           await GoodNight.create(goodNight)
+//         } else {
+//           await goodNight.update({ content: text })
+//         }
+//       } catch (e) {
+//         console.log(i, e)
+//       }
+//     }
+//   })
+// })
+
+const url = 'https://www.zhihu.com/question/360859696/answer/1487745111'
 
 crawler(url, html => {
   if (!html) {
@@ -52,28 +89,16 @@ crawler(url, html => {
     return
   }
   const $ = cheerio.load(html)
-  $('p', '.Post-RichText').each(async (i, e) => {
+  $('blockquote', '.RichContent-inner').each(async (i, e) => {
     const el = $(e)
     let text = el.text()
-    if (text.includes('.') || text.includes('．')) {
-      if (text.includes('.')) {
-        text = text.split('.')[1]
-      }
-      if (text.includes('．')) {
-        text = text.split('．')[1]
-      }
+    if (text.includes('.')) {
       if (!text.includes('晚安')) {
         text += '   晚安！'
       }
-      console.log(text)
       try {
-        let goodNight = await GoodNight.findOne({ where: { id: i + 110 } })
-        if (!goodNight) {
-          const goodNight = { content: text }
-          await GoodNight.create(goodNight)
-        } else {
-          await goodNight.update({ content: text })
-        }
+        const goodNight = { content: text }
+        await GoodNight.create(goodNight)
       } catch (e) {
         console.log(i, e)
       }
